8a32aa0d744a63bde542d0312ee27e0583c6d2a4
MarItzep <92335054+MarItzep@users.noreply.github.com>
2021-10-11 21:51:55 -0600

    sesión1

---
+++ clase1.py
@@ -0,0 +1,42 @@
+#tipos de datos String
+nombre = 'Marcos'
+nombre = 'Oscar'
+
+print(nombre)
+Nombre = 'Juan'
+apellido = 'Itzep'
+#tipos de datos int
+numero = 1 
+numero1= 2 
+numero2= 10
+#tipos de datos float 
+numero_float = 1.1
+#tipos booleans
+x= True
+y= False
+# #####concatenando con numeros 
+# print(numero,numero1)
+# ##suma
+# print(numero1+numero)
+# ##resta
+# print(numero2-numero1)
+
+############################--Hola mundo--###########################
+# nuevo= "Hola mundo"
+# print('Nombre 1: ', nombre , 'Nombre 2',Nombre)
+# print(nuevo)
+#### Concatenando##
+#print('Mi nombre es:',nombre+" "+'Mi Numero favorito es: '+str(numero))
+
+##Promedio##
+numero1= 10
+numero2= 2
+numero3= 4 
+numero4 = 5
+
+Promedio = (numero1+numero2+numero3+numero4)/4
+
+#multiplicacion
+resultante = numero1*numero2 
+#print(resultante)
+print(3*nombre)
